5a9a1d20ac678bf36c5399f14ef2c0fe91d053b2
adamkatsu <adamkatsu369@gmail.com>
2024-10-28 07:02:58 +0700

    add home-hero.js

---
+++ assets/home-hero.js
@@ -0,0 +1,26 @@
+var swiper = new Swiper(".swiper-thumbs", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".swiper-show", {
+  loop: true,
+  spaceBetween: 8,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
