2b46e28a564e6eea2201fdb2c7a5ed6cae4d1372
adamkatsu <adamkatsu369@gmail.com>
2024-10-29 21:37:06 +0700

    home product slider

---
+++ assets/swiper-product.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".swiper-product", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-product-next",
+    prevEl: ".swiper-product-prev",
+  },
+  loop: true,
+  spaceBetween: 24,
+});
