2a6ece33506254cb793d803f7333451cde91cd33
adamkatsu <adamkatsu369@gmail.com>
2024-10-31 07:04:00 +0700

    tutorials slider

---
+++ assets/swiper-tutorials.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".swiper-tutorials", {
+  slidesPerView: 3,
+  spaceBetween: 24,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-tutorial-next",
+    prevEl: ".swiper-tutorial-prev",
+  },
+});
