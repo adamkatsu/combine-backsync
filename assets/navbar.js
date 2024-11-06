d71adc68bfba3edb5340793e7e151f0f5d9d3d71
adamkatsu <adamkatsu369@gmail.com>
2024-11-06 22:02:26 +0700

    import fonts

--- assets/navbar.js
+++ assets/navbar.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
   const navbar = document.getElementById('section-navbar');
-  if (window.scrollY > 50) {
+  if (window.scrollY > 40) {
       navbar.classList.add('scrolled');
   } else {
       navbar.classList.remove('scrolled');
